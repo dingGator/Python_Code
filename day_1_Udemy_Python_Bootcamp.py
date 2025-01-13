aea1d5d97212a1714ef9590c3701eb14224261fa
Kim Chu <dingGator@users.noreply.github.com>
2025-01-13 11:40:24 -0500

    Add files via upload

---
+++ day_1_Udemy_Python_Bootcamp.py
@@ -0,0 +1,5 @@
+# make name of band
+print("Welcome to the Band Name Generator.")
+city_grew_up = input("What is the city you grew up in? \n")
+name_of_pet = input("What is the name of your pet? \n")
+print("The name of your band is " + city_grew_up + " " + name_of_pet + ".")
